4d0717ea0724a7b0d5fc880712cf999925eaf83d
Jeongbin <yon980824@gmail.com>
2022-01-11 11:15:47 +0900

    +-버튼 클릭 시 count 변경

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,15 +1,34 @@
 import React, { Component } from "react";
 
 class Habit extends Component {
+  state = {
+    count: 0,
+  };
+
+  handleIncrease = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  handleDecrease = () => {
+    const count = this.state.count - 1;
+    this.setState({ count: count < 0 ? 0 : count });
+  };
+
   render() {
     return (
       <li className="habit">
         <span className="habit-name">Reading</span>
-        <span className="habit-count">0</span>
-        <button className="habit-button habit-increase">
+        <span className="habit-count">{this.state.count}</span>
+        <button
+          className="habit-button habit-increase"
+          onClick={this.handleIncrease}
+        >
           <i className="fas fa-plus-square"></i>
         </button>
-        <button className="habit-button habit-decrease">
+        <button
+          className="habit-button habit-decrease"
+          onClick={this.handleDecrease}
+        >
           <i className="fas fa-minus-square"></i>
         </button>
         <button className="habit-button habit-delete">
